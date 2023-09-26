fdbdc1ab340683d610325a6f1c04a90fb0419f4d
muraterennar <muraterennar@hotmail.com>
2023-09-26 12:55:39 +0300

    bug fix

--- src/api/getRequest/getUserById.js
+++ src/api/getRequest/getUserById.js
@@ -1,6 +1,6 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 
-export const getUserById = createAsyncThunk("user", async (id) => {
+export const getUserById = createAsyncThunk("userbyid", async (id) => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${id}`);
     const data = await response.json();
     return data;
